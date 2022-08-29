a30161b20231d2d591f6e51f4446e43e8cec5e8d
Exordium <83123759+xSTronG30@users.noreply.github.com>
2022-08-29 15:24:29 +0200

    Update 1xs.lua

--- 1xs.lua
+++ 1xs.lua
@@ -43,7 +43,7 @@ end
 
 -- LEGENDS OF SPEED
 if game.PlaceId == 3101667897 or game.PlaceId == 3276265788 or game.PlaceId == 3232996272 then
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/xSTronG30/Fender-scripts/main/legend%20of%20speeed2%20x'),true))()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/xSTronG30/Fender-scripts/main/legend%20of%20speeed2%20xxX'),true))()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/xSTronG30/Fender-scripts/main/jumping%20auto',true))() -- jumpingCode
 else
 print("WRONG GAME")
